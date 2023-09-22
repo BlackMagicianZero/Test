2404d9d6ebe433c417090313f4b6beb750c22a2a
LCB <ckdqja5815@naver.com>
2023-09-23 00:01:28 +0900

    spine 작업

--- Assets/Characters/Enemies/Rabbit/Rabbit.cs
+++ Assets/Characters/Enemies/Rabbit/Rabbit.cs
@@ -1,92 +1,188 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine;
 using Spine.Unity;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(SpineTouchingDirections), typeof(SpineDamageable))]
 public class Rabbit : MonoBehaviour
 {
-    private SkeletonAnimation spineAnimation;
+    private SkeletonAnimation skeletonAnimation;
+    private string currentAnimation = "walk"; // 초기 애니메이션 설정
+
+    public float walkAcceleration = 3f;
+    public float maxSpeed = 3f;
+    public float walkStopRate = 0.05f;
+    public DetectionZone attackZone;
+    public DetectionZone cliffDetectionZone;
+
     private Rigidbody2D rb;
+    private SpineTouchingDirections touchingDirections;
+    private Animator animator;
+    private SpineDamageable damageable;
 
-    [SerializeField] private float walkSpeed = 2.0f;
+    public enum WalkableDirection { Right, Left }
 
-    private void Start()
+    private WalkableDirection _walkDirection;
+    private Vector2 walkDirectionVector = Vector2.right;
+
+    public WalkableDirection WalkDirection
     {
-        spineAnimation = GetComponent<SkeletonAnimation>();
+        get
+        {
+            return _walkDirection;
+        }
+        set
+        {
+            if (_walkDirection != value)
+            {
+                // 방향 뒤집기
+                gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y);
+
+                if (value == WalkableDirection.Right)
+                {
+                    walkDirectionVector = Vector2.right;
+                }
+                else if (value == WalkableDirection.Left)
+                {
+                    walkDirectionVector = Vector2.left;
+                }
+            }
+
+            _walkDirection = value;
+        }
+    }
+
+    public bool _hasTarget = false;
+
+    public bool HasTarget
+    {
+        get
+        {
+            return _hasTarget;
+        }
+        private set
+        {
+            _hasTarget = value;
+            animator.SetBool(AnimationStrings.hasTarget, value);
+        }
+    }
+
+    // CanMove 속성을 Spine 애니메이션 상태를 기반으로 설정
+    public bool CanMove
+    {
+        get
+        {
+            // Spine 애니메이션의 상태를 확인하여 CanMove를 설정
+            if (skeletonAnimation != null)
+            {
+                // "walk_animation_name"은 Spine 애니메이션의 걷기 애니메이션 이름으로 변경하세요.
+                return !skeletonAnimation.AnimationState.GetCurrent(0).Animation.Name.Equals("walk");
+            }
+            return false; // 예외 처리
+        }
+    }
+
+    public float AttackCooldown
+    {
+        get
+        {
+            return animator.GetFloat(AnimationStrings.attackCooldown);
+        }
+        private set
+        {
+            animator.SetFloat(AnimationStrings.attackCooldown, Mathf.Max(value, 0));
+        }
+    }
+
+    private void Awake()
+    {
         rb = GetComponent<Rigidbody2D>();
-        
-        // 시작 시 walk 애니메이션 재생
-        PlayWalkAnimation();
+        touchingDirections = GetComponent<SpineTouchingDirections>();
+        animator = GetComponent<Animator>();
+        damageable = GetComponent<SpineDamageable>();
+
+        // Spine 애니메이션 컴포넌트 초기화
+        skeletonAnimation = GetComponent<SkeletonAnimation>();
     }
 
-    private void Update()
+    void Update()
     {
-        // Walk 애니메이션을 플레이하며 양 옆으로 움직임
-        Move(walkSpeed);
+        // 스파인 애니메이션을 여기서 제어
+        // 예를 들어 HasTarget에 따라 애니메이션을 변경하려면:
 
-        // 움직임 로직을 추가하여 원하는 동작을 구현할 수 있습니다.
-        // 예를 들어, 공격 범위에 플레이어가 있는지 확인하고 공격 로직을 추가할 수 있습니다.
-        if (IsPlayerInAttackRange())
+        if (HasTarget)
         {
-            AttackPlayer();
+            SetAnimation("attack");
+        }
+        else
+        {
+            SetAnimation("walk");
+            Debug.Log("Setting animation to: " + "walk");
+
+        }
+
+        // AttackCooldown 및 기타 로직을 업데이트할 수 있습니다.
+    }
+
+    private void SetAnimation(string animationName)
+    {
+        if (skeletonAnimation != null && !animationName.Equals(currentAnimation))
+        {
+            skeletonAnimation.AnimationState.SetAnimation(0, animationName, true);
+            currentAnimation = animationName;
         }
     }
 
-    // 플레이어에게 피격될 때 호출하는 함수
-    public void TakeDamage()
+    private void FixedUpdate()
     {
-        // Hit 애니메이션 재생
-        PlayHitAnimation();
-    }
-
-    // 플레이어를 공격할 때 호출하는 함수
-    public void AttackPlayer()
-    {
-        // Attack 애니메이션 재생
-        PlayAttackAnimation();
-    }
-
-    // Walk 애니메이션 재생
-    private void PlayWalkAnimation()
-    {
-        if (spineAnimation != null)
+        if (touchingDirections.IsGrounded && touchingDirections.IsOnWall)
         {
-            spineAnimation.AnimationState.SetAnimation(0, "walk", true);
+            FlipDirection();
+        }
+
+        if (!damageable.LockVelocity)
+        {
+                        Debug.Log("Setting animation to: " + "walk");
+
+            if (CanMove && touchingDirections.IsGrounded)
+            {
+                // 최대 속도까지 가속
+                rb.velocity = new Vector2(
+                    Mathf.Clamp(rb.velocity.x + (walkAcceleration * walkDirectionVector.x * Time.fixedDeltaTime), -maxSpeed, maxSpeed),
+                    rb.velocity.y);
+            }
+            else
+                rb.velocity = new Vector2(Mathf.Lerp(rb.velocity.x, 0, walkStopRate), rb.velocity.y);
         }
     }
 
-    // Attack 애니메이션 재생
-    private void PlayAttackAnimation()
+    private void FlipDirection()
     {
-        if (spineAnimation != null)
+        if (WalkDirection == WalkableDirection.Right)
         {
-            spineAnimation.AnimationState.SetAnimation(0, "attack", false);
+            WalkDirection = WalkableDirection.Left;
+        }
+        else if (WalkDirection == WalkableDirection.Left)
+        {
+            WalkDirection = WalkableDirection.Right;
+        }
+        else
+        {
+            Debug.LogError("현재 걸을 수 있는 방향이 오른쪽 또는 왼쪽으로 설정되지 않았습니다.");
         }
     }
 
-    // Hit 애니메이션 재생
-    private void PlayHitAnimation()
+    public void OnHit(int damage, Vector2 knockback)
     {
-        if (spineAnimation != null)
+        rb.velocity = new Vector2(knockback.x, rb.velocity.y + knockback.y);
+    }
+
+    public void OnCliffDetected()
+    {
+        if (touchingDirections.IsGrounded)
         {
-            spineAnimation.AnimationState.SetAnimation(0, "hit", false);
+            FlipDirection();
         }
-    }
-
-    // 움직임 함수
-    private void Move(float speed)
-    {
-        // 이동 로직을 Rigidbody2D를 사용하여 구현
-        rb.velocity = new Vector2(speed, rb.velocity.y);
-    }
-
-    // 플레이어가 공격 범위에 있는지 확인하는 함수
-    private bool IsPlayerInAttackRange()
-    {
-        // 원하는 공격 범위 확인 로직 추가
-        // 예: 플레이어의 위치와 Rabbit의 위치를 비교하여 공격 범위에 있는지 확인
-        // 만약 공격 범위 내에 플레이어가 있다면 true 반환, 그렇지 않으면 false 반환
-        return false;
     }
 }
