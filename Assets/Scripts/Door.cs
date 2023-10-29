4f6604dc135133484cffe8ba980c464ecc3c2403
LCB <ckdqja581592@gmail.com>
2023-10-29 21:55:07 +0900

    중간발표전 최종 빌드

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -7,9 +7,17 @@ public class Door : MonoBehaviour
 {
     // 다음으로 이동할 씬의 이름
     public string nextSceneName;
+    private PlayerController thePlayer;
 
     private bool hasPlayerEntered = false;
+    private Animator anim;
+    public float distance = 3.0f;
 
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerController>();
+        anim = GetComponent<Animator>();
+    }   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!hasPlayerEntered && collision.CompareTag("Player"))
@@ -23,6 +31,14 @@ public class Door : MonoBehaviour
 
     private void Update()
     {
+        if(Vector3.Distance(transform.position, thePlayer.transform.position) < distance)
+        {
+            anim.SetBool("Open", true);
+        }
+        else
+        {
+            anim.SetBool("Open", false);
+        }
         // 현재 씬에 플레이어가 있는지 확인하고 RespawnArea로 이동
         if (hasPlayerEntered)
         {
